b7ac20a88fe4be8da70595c05d9b2024c46544cf
Ahnaf Khalique <ahnafkhalique@hotmail.com>
2023-03-08 17:24:45 +0000

    weather icons + initial progress done

--- src/components/sideTubes/description.js
+++ src/components/sideTubes/description.js
@@ -1,12 +1,25 @@
 // import preact
 import { h, render, Component } from 'preact';
-	
+
+import style from './style';
+import '../../assets/weather-icons/less/weather-icons'
+
 export default class WeatherDescription extends Component {
 
 	render() {
 		return (
 			<div>
-                <p>Weather DESCRIPTION</p>
+				<div class={style.desc_name}>
+					<h4>Mile End</h4>
+				</div>
+				<div class={style.desc_pic}>
+					<i class="wi wi-night-sleet"></i>
+				</div>
+				<div class={style.desc_text}>
+					<p>
+						"Clear skies without a trace of rain"
+					</p>
+				</div>
 			</div>
 		);
 	}
