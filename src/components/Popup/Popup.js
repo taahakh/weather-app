8e31283eb166f59b38ee63dc5a5c650f73f38d9a
Janoth <janoth14@gmail.com>
2023-03-09 11:15:05 +0000

    Delays V1

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,43 @@
+import { h, render, Component } from 'preact';
+
+export default class Popup extends Component {
+
+	render(contents) {
+		const styles = {
+			popup: {
+				position : 'fixed',
+				top : '0',
+				left : '0',
+				width : '100%',
+				height : '100vh',
+				backgroundColor : 'rgba(0, 0, 0, 0.2)',
+				color : '#000000',
+
+				display : 'flex',
+				justifyContent : 'center',
+				alignItems : 'center'
+			},
+			popupInner: {
+				position : 'relative',
+				padding : '32px',
+				width : '100%',
+				maxWidth : '640px',
+				backgroundColor: '#FFFFFF'
+			},
+			closeBtn: {
+				position : 'absolute',
+				top : '16px',
+				right : '16px'
+			}
+		};
+
+		return (contents.trigger) ? (
+			<div className="popup" style={styles.popup}>
+				<div className="popup-inner" style={styles.popupInner}>
+					<button className="close-btn" style={styles.closeBtn}>Close</button>
+					{ contents.children }
+				</div>
+			</div>
+		) : "";
+	}
+}
--- src/components/bottombar/index.js
+++ src/components/bottombar/index.js
@@ -1,12 +1,17 @@
 import { h, render, Component } from 'preact';
+import Popup from '../Popup/Popup';
 
 export default class BottomBar extends Component {
 
-    render() {
-        return (
+	render() {
+		return (
             <div>
-                <p>Bottom</p>
+				<button>Delays</button>
+				<Popup trigger={true}>
+					<h2>Delays</h2>
+					<p>This is to be filled with current and upcoming delays</p>
+				</Popup>
             </div>
-        );
-    }
+		);
+	}
 }
