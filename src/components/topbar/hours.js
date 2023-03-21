f8834f8456d250b473965e722be0ef2b2036d2e9
Busohabit <90647806+Busohabit@users.noreply.github.com>
2023-03-21 21:28:34 +0000

    Add files via upload

--- src/components/topbar/hours.js
+++ src/components/topbar/hours.js
@@ -1,30 +1,41 @@
-import Preact from 'preact';
-import style from './style';
-import React, {useState} from 'react';
-import "react-datepicker/dist/react-datepicker.css"
+// import Preact from 'preact';
+import style from './style.less';
+import { h, render, Component } from 'preact';
+// import React, {useState} from 'react';
+// import "react-datepicker/dist/react-datepicker.css"
 
-const HourStates = (props) => {
 
-    const initalStats = [{hour: '', temp:'', precipitation:'', windR:'', pressure:''}]
-    const [stats, setStats] = useState(initalStats);
+export default class HourStates extends Component {
 
-    return (
-        <div>
-            <ul>
-                {stats.map(item => (
-                            <li key = {item.hour}>
-                            <p>{item.hour}</p>
-                            <img src = {props.icon}/>
-                            <p>{item.temp}</p>
-                            <p>{item.precipitation}</p>
-                            <p>{item.windR}</p>
-                            <p>{item.pressure}</p>
-                        </li>          
+    // const initalStats = [{hour: '', temp:'', precipitation:'', windR:'', pressure:''}]
+    // const [stats, setStats] = useState(initalStats);
+
+    constructor(props) {
+        super(props);
+        this.state = {stats : [{hour: '19:00', temp:'20c', precipitation:'100%', windR:'50km/h', pressure:'30pascals'}]};
+    }
+
+
+        render() {
+
+            return (
+            <div class = {style.hours}>
+                <ul>
+                    {this.state.stats.map(item => (
+                                <li key = {item.hour}>
+                                    <img src = 'https://ibb.co/3yvSktT' class = {style.item}/>
+                                    <p class = {style.item}>{item.temp}</p>
+                                    <p class = {style.item}>{item.precipitation}</p>
+                                    <p class = {style.item}>{item.windR}</p>
+                                    <p class = {style.item}>{item.pressure}</p>
+                                    <p class = {style.item} >{item.hour}</p>
+                                </li>          
                     ))}
-            </ul>
-        </div>
-    );
-};
+                </ul>
+            </div>
+            );
+        }
 
-export default HourStates;
+
+};
     
--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -1,68 +1,60 @@
 
 import { h, render, Component } from 'preact';
 import Popup from '../Popup/Popup';
-import React, {useState} from 'react';
-import DatePicker from 'react-datepicker';
-import style from './style';
+// import React, {useState} from 'react';
+// import DatePicker from 'react-datepicker';
+import style from './style.less';
 
-import "react-datepicker/dist/react-datepicker.css"
+
+// import "react-datepicker/dist/react-datepicker.css"
 
 export default class TopBar extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			buttonPopup: false
+			buttonPopup: false,
+			days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 		};
 	}
 
-    render() {
-		const DatePicker = () => {
-			const [date, setDate] = useState('')
-			const dateInputRef = useRef(null);
-			const handleChange = (e) => {
-			setDate
-    };
+	render() {
+
+		// const DatePicker = () => {
+		// 	const [date, setDate] = useState('')
+		// 	const dateInputRef = useRef(null);
+		// 	const handleChange = (e) => {
+		// 	setDate
+		// 	};
 
 
-};
+		// };
+
   //  const [startDate, setStartDate] = useState(new Date());
-       return (
+
+		const today = new Date().getDay();
+		const daysList = [];
+
+		for (let i = 0; i < 7; i++) {
+			const dayIndex = (today + i) % 7;
+			const day = this.state.days[dayIndex];
+			daysList.push(<div class={style.dayInner} key={i} onClick={() => this.props.days.showDayWeather(i)}>{day}</div>);
+		}
+
+		return (
        			<div className="bottom-bar">
-				<table border="1">
-				<tr>
-					<td colspan="7" align = "center" >
-						<div>
-       						<input type="date"/>
-       					</div>
-       				</td>
-       			</tr>
-       			<tr>
-       			<td class={ style.day }>Thur</td>
-       			<td class={ style.day }>Tue</td>
-       			<td class={ style.day }>Wed</td>
-       			<td class={ style.day }>Thur</td>
-       			<td class={ style.day }>Fri</td>
-       			<td class={ style.day }>Sat</td>
-       			<td class={ style.day }>Sun</td>
-       			</tr>
-				</table>
-       			<div>
+					{/* <div align="center">
+						<input type="date" class={style.calendar}/>
+					</div> */}
+					<div class={style.dayOuter}>
+						{daysList}
+					</div>
+				   </div>
 
-       			</div>
-       				<button onClick={() => this.setState({ buttonPopup: true })}>
-       					Location
-       				</button>
-       				<Popup trigger={this.state.buttonPopup} onClose={() => this.setState({ buttonPopup: false })}>
-       					<h3>Location</h3>
-       					<p>Insert Location Here...</p>
-       				</Popup>
 
-       			</div>
-
-       		);
+		);
 
 
 
-    }
+	}
 }
